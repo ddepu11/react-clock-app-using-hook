4dee5a3fb9b9f07f197e51597dd1526badbddd1e
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-04 18:10:27 +0530

    styled setinter all done

---
+++ src/Clock.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const Clock = () => {
+  let time = new Date().toLocaleTimeString();
+
+  const [currentTime, setCurrentTime] = useState(time);
+
+  setInterval(() => {
+    setCurrentTime(new Date().toLocaleTimeString());
+  }, 1000);
+
+  return (
+    <div className="clock">
+      <span>{currentTime}</span>
+    </div>
+  );
+};
+
+export default Clock;
